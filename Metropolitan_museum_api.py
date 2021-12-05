5f04c52199a4a935d73439bc7131e54859892b1b
Saatwik Mehta <saatwikmehta@gmail.com>
2021-12-05 17:07:47 +0530

    Comments added

--- Metropolitan_museum_api.py
+++ Metropolitan_museum_api.py
@@ -6,38 +6,43 @@ data = []
 
 def func(v):
     # v is a list type
+
     dict_val = []
-    for i in v:
-        if isinstance(i, dict):  # [12,14,2345,232454] or [key:v, key:{key:v}] or [12,14,2345,232454,key:v]
-            for j in i:
-                if isinstance(i[j], dict):
-                    dict_val.extend(i[j].values())
+    for item in v:
+        if isinstance(item, dict):
+            for item_value in item:
+                if isinstance(item[item_value], dict):
+                    dict_val.extend(item[item_value].values())
                 else:
-                    dict_val.append(i[j])
+                    dict_val.append(item[item_value])
 
         else:
             return ','.join(v)
     return ','.join([str(a) for a in dict_val])
 
 
+# Fetching first 100 object-IDs
 objects = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/objects")
 d_data = objects.json()
-# d_data['objectIDs']
-data_cent=d_data['objectIDs'][0:100]
+data_cent = d_data['objectIDs'][0:100]
 
+# Fetching the data of first 100 objects with their ID
 for j in data_cent:
 
     result = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/objects/" + str(j))
 
-    # fetching data in JSON format
+    # Saving data in JSON format
     response_data = result.json()
 
     for i in response_data:
         if isinstance(response_data[i], list):
+            # Using function -> func to retrieve data values from dictionary
             response_data[i] = func(response_data[i])
 
+    # Saving the modified data
     data.append({i: response_data[i] for i in response_data})
-
+# Creating dataframe of the modified data
 df = pandas.DataFrame(data)
 
-df.to_csv("Metropolitan_Museum_api.csv", encoding="utf-8-sig", index=False, na_rep="None" )
+# Saving data in CSV file
+df.to_csv("Metropolitan_Museum_api.csv", encoding="utf-8-sig", index=False, na_rep="None")
