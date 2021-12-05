09c47d7e472e79fa7c1c4245ce55a7b9e1805921
Saatwik Mehta <saatwikmehta@gmail.com>
2021-12-05 15:28:35 +0530

    Metropolitan_museum_api data of 100 objects

--- Metropolitan_museum_api.py
+++ Metropolitan_museum_api.py
@@ -1,33 +1,43 @@
-import json
-import sys
 import pandas
 import requests
-import csv
 
 data = []
 
 
-
 def func(v):
+    # v is a list type
     dict_val = []
     for i in v:
-        if isinstance(i, dict):
+        if isinstance(i, dict):  # [12,14,2345,232454] or [key:v, key:{key:v}] or [12,14,2345,232454,key:v]
+            for j in i:
+                if isinstance(i[j], dict):
+                    dict_val.extend(i[j].values())
+                else:
+                    dict_val.append(i[j])
 
-            print([str(i) for i in i.values()])
-            return ','.join([str(i) for i in i.values()])
+        else:
+            return ','.join(v)
+    return ','.join([str(a) for a in dict_val])
 
 
-for j in range(1, 11):
+objects = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/objects")
+d_data = objects.json()
+# d_data['objectIDs']
+data_cent=d_data['objectIDs'][0:100]
+
+for j in data_cent:
+
     result = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/objects/" + str(j))
+
+    # fetching data in JSON format
     response_data = result.json()
+
     for i in response_data:
         if isinstance(response_data[i], list):
-            if len(response_data[i]):
-                response_data[i]=func(response_data[i])
-            else:
-                response_data[i]=','.join(response_data[i])
+            response_data[i] = func(response_data[i])
+
     data.append({i: response_data[i] for i in response_data})
 
 df = pandas.DataFrame(data)
 
-df.to_csv("D:/csv_api.csv", encoding="utf-8-sig")
+df.to_csv("Metropolitan_Museum_api.csv", encoding="utf-8-sig", index=False, na_rep="None" )
